47077e0dc11f21cd4b318deee96fbdf6f3ee3b24
jll2884 <simplylopez7@gmail.com>
2019-05-30 15:32:33 -0400

    new updates

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+       <b-nav tabs>
+           <b-nav-item disabled><b>Fancy Pop Quiz</b></b-nav-item>
+           <b-nav-item disabled>Counter 4/10</b-nav-item>
+
+       </b-nav>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <b-jumbotron>
+            <template slot="lead">
+              {{Currentquestion.question}}
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+               List Of Answers
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            Currentquestion: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
